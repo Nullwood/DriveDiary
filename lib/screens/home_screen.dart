165f3836d768d9c7231ccfc417113659ffa4b475
Nullwood <tallahassee.wss@gmail.com>
2025-10-27 09:27:24 +0500

    init

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _increment10() {
+    setState(() {
+      _counter += 10;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DriveDiary'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Ваш пробег: '),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            child: const Icon(Icons.add_box),
+            backgroundColor: Colors.grey,
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _increment10,
+            child: const Icon(Icons.add),
+            backgroundColor: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
